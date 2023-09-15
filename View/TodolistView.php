99176414e5cadb9d87bc59f4145c220c10503828
haris2303 <hrsccf102@gmail.com>
2023-09-15 15:12:20 +0900

    View

---
+++ View/TodolistView.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace View {
+
+    class TodolistView
+    {
+
+        public function showTodolist(): void
+        {
+        }
+
+        public function addTodolist(): void
+        {
+        }
+
+        public function removeTodolist(): void
+        {
+        }
+    }
+}
